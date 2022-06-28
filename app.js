3d2a0bdbe9532e4353d9d85204084a0d3603f12c
Nicholas_Huang713 <nhuang713@gmail.com>
2022-06-27 20:14:50 -0700

    update text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({msg: 'Hello Pokemon Trainer'})
+    res.json({msg: 'Hello Nick'})
 })
 
 app.listen(3000);
