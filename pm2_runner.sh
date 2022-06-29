ce0230875de12061d4e7d27855e379e0bfa69e4e
Nick Huang <50302822+Nicholas-Huang713@users.noreply.github.com>
2022-06-28 21:32:25 -0700

    Update pm2_runner.sh

--- pm2_runner.sh
+++ pm2_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! type pm2 > /dev/null
 then
-    sudo npm install -g pm2 && pm2 start ./app.js --name backend
+    sudo npm install -g pm2 && pm2 start ./app.js 
 else
-    pm2 restart backend
+    pm2 restart app.js
 fi
