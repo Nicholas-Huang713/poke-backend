2fdac5d9b52a27b1e6282442d16822cb09ed55ef
Nicholas_Huang713 <nhuang713@gmail.com>
2022-06-28 21:27:52 -0700

    update api

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({msg: 'Hello Nick'})
+    res.json({msg: 'Hello Pokemon Cards!'})
 })
 
 app.listen(3000);
